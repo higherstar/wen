e8a17ea26c90767362bda0043577b651f42f2bc4
unknown <alexandar.nikita@zoho.com>
2017-08-16 16:16:08 +0100

    update server socket

--- server/socket.server.js
+++ server/socket.server.js
@@ -1,21 +1,15 @@
 const App = require('koa');
-
 const { SubscriptionServer } = require('subscriptions-transport-ws');
 const subscriptionManager = require('./graphql/subscriptionManager');
 const config = require('../common/config.js');
 const cookie = require('cookie');
 const jwtService = require('./services/token.service');
 const logger = require('./utils/logger.js');
-
-
 const app = new App();
-
 const port = config.socket.port;
-
 const server = app.listen(port, () => {
   logger.info(`Socket server listening on port:${port} in ${process.env.NODE_ENV} mode`);
 });
-
 module.exports.subscriptionServer = new SubscriptionServer(
   {
     onConnect: async (connectionParams, websocket) => {
