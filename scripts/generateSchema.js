5e4e85f3fd9a8b68a0a2235b1d80f8ce6672440f
alexandarnikita <30359573+alexandarnikita@users.noreply.github.com>
2017-08-16 16:30:16 +0100

    Update generate

--- scripts/generateSchema.js
+++ scripts/generateSchema.js
@@ -2,12 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const { buildSchema, introspectionQuery } = require('graphql/utilities');
 const { graphql } = require('graphql');
-
 const loadTextFile = require('../common/loadTextFile.js');
-
 const schemaString = loadTextFile(path.join(__dirname, '../common/graphql/schema.graphqls'));
 const schema = buildSchema(schemaString);
-
 graphql(schema, introspectionQuery)
   .then((result) => {
     fs.writeFileSync(
