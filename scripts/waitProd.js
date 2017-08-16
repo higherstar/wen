9510f7fbf037564d6bbdbf1eb03e7dd23ced1e33
alexandarnikita <30359573+alexandarnikita@users.noreply.github.com>
2017-08-16 16:28:36 +0100

    update waitProd

--- scripts/waitProd.js
+++ scripts/waitProd.js
@@ -1,17 +1,13 @@
 const waitOn = require('wait-on');
 const config = require('../common/config.js');
 const ora = require('ora');
-
-
 const opts = {
   resources: [
     `http-get://${config.server.host}:${config.server.port}`,
     `tcp:${config.db.host}:${config.db.port}`
   ],
 };
-
 const spinner = ora('Please wait for environment setup').start();
-
 waitOn(opts, (err) => {
   if (err) {
     spinner.fail(err);
